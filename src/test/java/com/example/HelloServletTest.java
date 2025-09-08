d69a810c858d019d3dc58572ec508c522eee9933
santhoshannadurai6256-dotcom <santhoshannadurai6256@gmail.com>
2025-09-09 00:20:34 +0530

    Create HelloServletTest.java

---
+++ src/test/java/com/example/HelloServletTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HelloServletTest {
+    @Test
+    void sampleTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
